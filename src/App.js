03ef26849e29803a8f029b8693255ba90d9c966f
wazimuhb <hesbonosoro1@gmail.com>
2021-12-02 19:07:39 +0300

    using Sidebar and Feed components while displaying them using flex property

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import Feed from "./components/Feed";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
 	return (
 		<AppWrapper>
 			<Header />
+			<div className="app__body">
+				<Sidebar />
+				<Feed />
+			</div>
 		</AppWrapper>
 	);
 }
 
 const AppWrapper = styled.div`
 	background-color: #f1f2f5;
+	.app__body {
+		display: flex;
+	}
 `;
 export default App;
