37017df7575395ba050befd8648193e48336b77c
wazimuhb <hesbonosoro1@gmail.com>
2021-12-02 18:07:58 +0300

    Header with styled-components

---
+++ src/components/Header.js
@@ -0,0 +1,133 @@
+import React from "react";
+import styled from "styled-components";
+import {
+	Search,
+	Home,
+	SubscriptionsOutlined,
+	StorefrontOutlined,
+	SupervisedUserCircle,
+	Forum,
+	NotificationsActive,
+	Add,
+	ExpandMore,
+} from "@material-ui/icons";
+import { IconButton, Avatar } from "@material-ui/core";
+
+const Header = () => {
+	return (
+		<HeaderWrapper>
+			<HeaderLeft>
+				<img src="logo192.png" alt="Social Network" />
+			</HeaderLeft>
+			<HeaderInput>
+				<Search />
+				<input placeholder="Search Network" type="text" />
+			</HeaderInput>
+			<HeaderCenter>
+				<div className="header__option header__option--active">
+					<Home fontsize="large" />
+				</div>
+				<div className="header__option">
+					<SubscriptionsOutlined fontsize="large" />
+				</div>
+				<div className="header__option">
+					<StorefrontOutlined />
+				</div>
+				<div className="header__option">
+					<SupervisedUserCircle fontsize="large" />
+				</div>
+			</HeaderCenter>
+			<HeaderRight>
+				<div className="header__info">
+					<Avatar src="https://i.postimg.cc/RFYn9wvk/portfolio.png" />
+					<h4>Wazimu</h4>
+				</div>
+				<IconButton>
+					<Add />
+				</IconButton>
+				<IconButton>
+					<Forum />
+				</IconButton>
+				<IconButton>
+					<NotificationsActive />
+				</IconButton>
+				<IconButton>
+					<ExpandMore />
+				</IconButton>
+			</HeaderRight>
+		</HeaderWrapper>
+	);
+};
+
+const HeaderWrapper = styled.div`
+	display: flex;
+	padding: 15px 20px;
+	justify-content: space-between;
+	align-items: center;
+	position: sticky;
+	background-color: white;
+	z-index: 100;
+	top: 0;
+	box-shadow: 0px 5px 8px -9px rgba(0, 0, 0, 0.75);
+`;
+const HeaderLeft = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	img {
+		height: 40px;
+	}
+`;
+const HeaderInput = styled.div`
+	display: flex;
+	align-items: center;
+	background-color: #eff2f5;
+	padding: 10px;
+	margin-left: 10px;
+	border-radius: 33px;
+	input {
+		border: none;
+		background-color: transparent;
+		outline-width: 0;
+	}
+`;
+const HeaderCenter = styled.div`
+	display: flex;
+	flex: 1;
+	justify-content: center;
+	.header__option {
+		display: flex;
+		align-items: center;
+		padding: 10px 30px;
+		cursor: pointer;
+		.MuiSvgIcon-root {
+			color: gray;
+		}
+		&:hover {
+			background-color: #eff2f5;
+			border-radius: 10px;
+			align-items: center;
+			padding: 0 30px;
+			border-bottom: none;
+			.MuiSvgIcon-root {
+				color: #2e81f4;
+			}
+		}
+	}
+	.header__option--active {
+		border-bottom: 4px solid #2e81f4;
+		.MuiSvgIcon-root {
+			color: #2e81f4;
+		}
+	}
+`;
+const HeaderRight = styled.div`
+	display: flex;
+	.header__info {
+		display: flex;
+		align-items: center;
+		h4 {
+			margin-left: 10px;
+		}
+	}
+`;
+export default Header;
