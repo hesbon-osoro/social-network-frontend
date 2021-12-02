9499b634e74d2eadcc21c4c7f603ab9c09ace9b0
wazimuhb <hesbonosoro1@gmail.com>
2021-12-02 23:58:59 +0300

    Widget component

---
+++ src/components/Widget.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const Widget = () => {
+	return (
+		<WidgetWrapper>
+			<iframe
+				src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmyprofile&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+				width="340"
+				height="1500"
+				style={{ border: "none", overflow: "hidden" }}
+				scrolling="no"
+				frameborder="0"
+				allow="encrypted-media"
+				title="Facebook Widget"
+			></iframe>
+		</WidgetWrapper>
+	);
+};
+const WidgetWrapper = styled.div``;
+
+export default Widget;
