10ea5818eecbf77b07c6c9f267c14e8d9b1eb622
wazimuhb <hesbonosoro1@gmail.com>
2021-12-03 14:37:52 +0300

    using Context API in the top level to provide the app with a user

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import { StateProvider } from "./StateProvider";
+import reducer, { initialState } from "./Reducer";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<StateProvider initialState={initialState} reducer={reducer}>
+			<App />
+		</StateProvider>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
