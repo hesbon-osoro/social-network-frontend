3889915ad0d00fda832bd59f365ad788025649d5
wazimuhb <hesbonosoro1@gmail.com>
2021-12-03 22:53:29 +0300

    fetching data from the backend and populating to frontend

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,29 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Stories from "./Stories";
 import styled from "styled-components";
 import Messenger from "./Messenger";
 import Post from "./Post";
+import axios from "../axios";
 
 const Feed = () => {
+	const [posts, setPosts] = useState([]);
+
+	const syncFeed = () => {
+		axios.get("/posts").then((res) => {
+			console.log(res.data);
+			setPosts(res.data);
+		});
+	};
+	useEffect(() => {
+		syncFeed();
+	}, []);
 	return (
 		<FeedWrapper>
 			<Stories />
 			<Messenger />
-			<Post
-				profilePic="https://i.postimg.cc/SRRNxqs3/michael-dam-m-EZ3-Po-FGs-k-unsplash.jpg"
-				message="I found this art worth sharing. I hope you like it."
-				timestamp="2021-12-03T08:06:34.036Z"
-				imgName="https://i.postimg.cc/fR2KxrH0/lady.jpg"
-				a
-				username="Wazimu"
-			/>
-			<Post
-				profilePic="https://i.postimg.cc/RFYn9wvk/portfolio.png"
-				message="This is an awesome hair style. "
-				timestamp="2021-12-03T08:05:33.429Z"
-				imgName="https://i.postimg.cc/G3gmSBgC/cesar-rincon-XHVp-Wcr5gr-Q-unsplash.jpg"
-				username="Hesbon"
-			/>
+			{posts.map((post) => (
+				<Post
+					key={post.timestamp}
+					profilePic={post.avatar}
+					messafe={post.text}
+					timestamp={post.timestamp}
+					imgName={post.imgName}
+					username={post.user}
+				/>
+			))}
 		</FeedWrapper>
 	);
 };
