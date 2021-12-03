ca51844ed96ff1666f3699dd39ea9b02a56c120f
wazimuhb <hesbonosoro1@gmail.com>
2021-12-03 11:58:03 +0300

    using Post component and passing data to it

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,12 +2,28 @@ import React from "react";
 import Stories from "./Stories";
 import styled from "styled-components";
 import Messenger from "./Messenger";
+import Post from "./Post";
 
 const Feed = () => {
 	return (
 		<FeedWrapper>
 			<Stories />
 			<Messenger />
+			<Post
+				profilePic="https://i.postimg.cc/SRRNxqs3/michael-dam-m-EZ3-Po-FGs-k-unsplash.jpg"
+				message="Awesome post on Animation. Loved it"
+				timestamp="2021-12-03T08:06:34.036Z"
+				imgName="https://i.postimg.cc/fR2KxrH0/lady.jpg"
+				a
+				username="Wazimu"
+			/>
+			<Post
+				profilePic="https://i.postimg.cc/RFYn9wvk/portfolio.png"
+				message="Awesome post on Animation. Loved it"
+				timestamp="2021-12-03T08:05:33.429Z"
+				imgName="https://i.postimg.cc/G3gmSBgC/cesar-rincon-XHVp-Wcr5gr-Q-unsplash.jpg"
+				username="Hesbon"
+			/>
 		</FeedWrapper>
 	);
 };
