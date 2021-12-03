b148ecf8f3dcb9944b7d97e85a2ac54a90a49704
wazimuhb <hesbonosoro1@gmail.com>
2021-12-03 13:13:45 +0300

    Login component

---
+++ src/components/Login.js
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
+
+const Login = () => {
+	const signIn = () => {
+		auth
+			.signInWithPopup(provider)
+			.then((result) => console.log(result))
+			.catch((error) => alert(error.message));
+	};
+	return (
+		<LoginWrapper>
+			<div className="login__logo">
+				<img src="logo512.png" alt="login" />
+				<h1>Social Network</h1>
+			</div>
+			<Button type="submit" className="login__btn" onClick={signIn}>
+				Sign In
+			</Button>
+		</LoginWrapper>
+	);
+};
+
+const LoginWrapper = styled.div`
+	display: grid;
+	place-items: center;
+	height: 100vh;
+	width: 100vw;
+
+	.login__logo {
+		display: flex;
+		flex-direction: column;
+		img {
+			object-fit: contain;
+			height: 150px;
+			max-width: 200px;
+		}
+	}
+	.login__btn {
+		width: 300px;
+		background-color: #2e81f4;
+		color: #eff2f5;
+		font-weight: 800;
+		&:hover {
+			background-color: white;
+			color: #2e81f4;
+		}
+	}
+`;
+
+export default Login;
