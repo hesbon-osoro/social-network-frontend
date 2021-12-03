da16cacf15b241faf1730ee45166c90fd19651ad
wazimuhb <hesbonosoro1@gmail.com>
2021-12-03 14:39:36 +0300

    using Context API to get a user for logging

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,12 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import { Button } from "@material-ui/core";
 import { auth, provider } from "../firebase";
+import { useStateValue } from "../StateProvider";
+import { actionTypes } from "../Reducer";
 
 const Login = () => {
+	const [{}, dispatch] = useStateValue();
+
 	const signIn = () => {
 		auth
 			.signInWithPopup(provider)
-			.then((result) => console.log(result))
+			.then((result) => {
+				console.log(result);
+				dispatch({ type: actionTypes.SET_USER, user: result.user });
+			})
 			.catch((error) => alert(error.message));
 	};
 	return (
